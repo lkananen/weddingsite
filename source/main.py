4d97fccaaa61857f787953b9688a0a4111512ed0
Lauri Kananen <lauri.kananen@gmail.com>
2022-01-04 16:16:37 +0200

    Description section added to API docs.

--- source/main.py
+++ source/main.py
@@ -1,13 +1,42 @@
 from fastapi import FastAPI, status
 
-app = FastAPI()
-# Note! Automatically defines paths /docs and /redoc
+description = """
+## Description
+TODO (_not implemented_)
 
-@app.get("/")
+### Automatically generated paths
+- **/docs**: This documentation file.
+- **/redoc**: Alternative documentations.
+- **/openapi.json**: OpenAPI schema.
+"""
+
+tags_metadata = [
+    {
+        "name": "root",
+        "description": "Main site"
+    },
+    {
+        "name": "health",
+        "description": "Web server health status."
+    }
+]
+
+app = FastAPI(
+    title="Documentation of the site",
+    version="0.1.0",
+    description=description,
+    openapi_tags=tags_metadata,
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
+
+
+@app.get("/", tags=["root"])
 async def root():
     return {"message": "Hello!"}
 
 
-@app.get("/health", status_code=status.HTTP_200_OK)
+@app.get("/health", status_code=status.HTTP_200_OK, tags=["health"])
 async def health_check():
     return {"healtcheck": "ok"}
