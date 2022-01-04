5eee8783dcba40b7bfaa2ea283ca5f1a59325430
Lauri Kananen <lauri.kananen@gmail.com>
2022-01-04 15:01:55 +0200

    Healthcheck added.

--- source/main.py
+++ source/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
-
+# Note! Automatically defines paths /docs and /redoc
 
 @app.get("/")
 async def root():
     return {"message": "Hello!"}
+
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+async def health_check():
+    return {"healtcheck": "ok"}
