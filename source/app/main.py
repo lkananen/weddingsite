15acf260483561a2c4a77b77955be214050aeff2
Lauri Kananen <lauri.kananen@gmail.com>
2022-08-06 01:25:09 +0300

    Static path corrected.

--- source/app/main.py
+++ source/app/main.py
@@ -94,7 +94,7 @@ async def root(request: Request):
 # Wedding program
 @app.post("/program")
 def generate(data: str):
-    img = Image.open("/app/static/wedding.jpg")
+    img = Image.open("/static/wedding.jpg")
     img_buffer = io.BytesIO()
     img.save(img_buffer, "JPEG")
     img_buffer.seek(0)
